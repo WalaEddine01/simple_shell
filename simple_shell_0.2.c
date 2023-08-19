37cca2343a72df472c1406e4fac8e5362bef19fb
Ezzeddin Essameddin <ej_dozze95@outlook.com>
2023-08-19 04:36:34 -0700

    create a new file and fix minor error in _gitline function

---
+++ simple_shell_0.2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - Simple shell
+ * @argc: number of args
+ * @argv: array of strings
+ * @envp: array of strings environments
+ * Return: 0 or -1
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+	char *stk[MAX_ARGS], *line = NULL;
+
+	if (argc != 1)
+	{
+		return (0);
+	}
+	while (1)
+	{
+		_puts("$ ");
+		line = _getline(line);
+		_strtok(line, stk);
+		if (stk[1] != NULL)
+		_fork(stk, stk, envp, argv);
+		else
+		_fork2(stk, stk, envp, argv);
+	}
+	free(line);
+	return (0);
+}
