ac13614a898920823c28165edf1e1b5e72d45e4b
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-19 17:01:02 +0000

    writing command witout path

--- find_PATH.c
+++ find_PATH.c
@@ -9,19 +9,25 @@ char *_which(char *command, char **env)
 {
 	char *path = _getenv("PATH", env);
 	char *token;
-	char *full_path = malloc(_strlen(command) + 256);
+	char *full_path = NULL;
 	struct stat st;
 
 	token = strtok(path, ":");
 	while (token != NULL)
 	{
+		full_path = malloc(_strlen(token) + 1 + _strlen(command) + 1);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			exit(1);
+		}
 		_strcpy(full_path, token);
 		_strcat(full_path, "/");
 		_strcat(full_path, command);
 		if (stat(full_path, &st) == 0)
 			return (full_path);
+		free(full_path);
 		token = strtok(NULL, ":");
 	}
-	free(full_path);
-	return (0);
+	return (NULL);
 }
