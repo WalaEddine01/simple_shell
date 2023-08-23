cc4242abed37661db71d02f3374b923fff8706d5
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-23 10:24:58 +0000

    TEST

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -8,12 +8,16 @@
 int main(int argc, char *argv[])
 {
 	char *stk[10], *av[] = {" ", NULL}, *line = NULL;
+	int space;
 	(void)argc;
 
 	while (1)
 	{
 		_puts("$ ");
 		line = _getline(line);
+		space = test_white_space(line);
+		if (space == 1)
+			continue;
 		_strtok(line, stk);
 		_fork2(stk, av, argv);
 	}
