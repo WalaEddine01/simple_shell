c8f9d880233959014171f195d5e2658aec430b46
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-23 12:09:30 +0000

    TEST

---
+++ simple_shell_0.3.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * main - Simple shell
+ * @argc: number of args
+ * @argv: array of strings
+ * @envp: array of strings environments
+ * Return: 0 or -1
+ */
+int main(int argc, char *argv[], char **envp)
+{
+	char *stk[10], *av[] = {NULL}, *line = NULL, *cmd = NULL;
+	int ext, space;
+
+	stk[0] = NULL;
+	if (argc != 1)
+		return (0);
+	while (1)
+	{
+		_puts("$ ");
+		line = _getline(line);
+		space = test_white_space(line);
+		if (space == 1)
+			continue;
+		ext = shexit(line);
+		if (ext == 0)
+			continue;
+		cmd = _which(line, envp);
+		if (cmd == NULL)
+		{
+			_fork(stk, av, environ, argv);
+			continue;
+		}
+		_strtok(cmd, stk);
+		_fork(stk, stk, environ, argv);
+	}
+	if (cmd != NULL)
+		free(cmd);
+	free(line);
+	return (0);
+}
